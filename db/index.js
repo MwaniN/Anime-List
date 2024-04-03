3227586bce75974b8045dffe43762bdbe468bc29
MwaniN <brooklynewyork@gmail.com>
2024-04-02 23:41:06 -0400

    DB connected successfully

--- db/index.js
+++ db/index.js
@@ -2,3 +2,17 @@ const pg = require('pg')
 const config = require('../config.js');
 
 const client = new pg.Client(config);
+
+var connectIt = async function () {
+  await client.connect();
+  return "db is connected";
+}
+
+connectIt().then((data) => {console.log(data)});
+
+async function currDBTest () {
+  let result = await client.query('SELECT current_database();')
+  return result;
+}
+
+currDBTest().then((data) => {console.log(data.rows[0])})
