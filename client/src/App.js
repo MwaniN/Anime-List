a1827d7ac5ac657a6c3cd26054b5472cb04c771f
MwaniN <brooklynewyork@gmail.com>
2024-04-03 14:08:13 -0400

    Small updates and structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import MainPage from "./MainPage.jsx"
 
+// make a state that either shows Main Page or Collection page with conditional arguments in the return statement here
+// to display each
+
 const App = () =>{
     return (
         <>
