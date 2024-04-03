7a38aaa4ac8e748dd43f2320f8fd1ab19128800f
MwaniN <brooklynewyork@gmail.com>
2024-04-03 19:26:44 -0400

    Frontend architecture and notes

--- client/src/AnimeCard.jsx
+++ client/src/AnimeCard.jsx
@@ -10,19 +10,53 @@ import React, { useState } from 'react';
 // title_english
 // title
 // images.jpg.image_url
+// summary also
 
 // If there's no English title use the Japanese one, change the logic
 // add a toggle for NSFW results?
 
+// future enhancement - make the number of votes be formatted
+// future enhancement - limit text overflow for synopsis
+// future enhancement - make images load first before rendering the div
 
+// on handleSubmit -> use axios to add this particular anime's data to the corresponding collection in the DB
 
 
 export default function AnimeCard ({ result }) {
 
-  // console.log(result, " result from inside the anime card!")
 
-  return <div anime-card>
+  function handleSubmit(e){
+    e.preventDefault();
+  }
+
+  return <div className="anime-card">
     <img className="image" src={result.images.jpg.image_url} alt={`Image of the ${result.title_english} anime`} />
-    <span className="title">Title: {result.title_english}</span>
+    <span className="title">Title: {
+      function(){
+        if (result.title_english){
+          return result.title_english;
+        } else {
+          return result.title;
+        }
+      }()
+    }</span>
+    <span className="score">Average Score: {result.score}</span>
+    <span className="votes">Votes: {result.scored_by}</span>
+    <span className="status">Status: {result.status}</span>
+    <span className="year">Aired: {result.aired.string}</span>
+    <span className="studios">Studio: {
+      function (){
+        let studios = result.studios.map(
+          (studio) => {
+            return studio.name
+          }
+        )
+        return studios.join(', ');
+      }()
+    }</span>
+    <div className="synopsis">Synopsis: {result.synopsis}</div>
+    <form method="post" onSubmit={handleSubmit}>
+      <button type="submit">Add to Collection</button>
+    </form>
   </div>
 }
