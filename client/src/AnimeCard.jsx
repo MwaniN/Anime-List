ed9c29222bdccf3ec4ed4df2e2cb71133ff66dc4
MwaniN <brooklynewyork@gmail.com>
2024-04-03 15:29:23 -0400

    API built out to start

--- client/src/AnimeCard.jsx
+++ client/src/AnimeCard.jsx
@@ -1,6 +1,20 @@
 import React, { useState } from 'react';
 
-export default function AnimeCard () {
+
+// Use these params
+// loop through genres array and for each object take name
+// mal_id = key
+// score
+// scored_by
+// status
+// title_english
+// title
+// images.jpg.image_url
+
+
+
+
+export default function AnimeCard ({ result }) {
 
   return <div anime-card>
     This is the anime card
--- client/src/ResultList.jsx
+++ client/src/ResultList.jsx
@@ -1,10 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AnimeCard from './AnimeCard.jsx';
+import axios from 'axios'
 
 export default function ResultList ({ queryString }) {
 
+  const [resultList, setResultList] = useState('');
+  const result = queryString;
+
+  useEffect( () => {
+    axios.get(`https://api.jikan.moe/v4/anime?q=${queryString}`).then(
+      (results) => {
+        setResultList(results.data.data)
+      }
+    )
+
+  }, [])
+
+  if (resultList.length > 0) {
+    console.log(resultList)
+  }
+
   return <div className="result-list">
     Results for {queryString}
-    <AnimeCard />
+    <AnimeCard result={result} />
   </div>
 }
